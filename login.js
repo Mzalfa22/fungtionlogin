54ba0b6405ce7672c33ca8900a9908a572c4688f
Mzalfa22 <141709401+Mzalfa22@users.noreply.github.com>
2023-10-04 14:54:45 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+function login(username, pass) {
+  console.log("----------- ");
+  console.log("Login");
+  console.log("----------- ");
+  console.log("Username :" + username);
+  console.log("Pass :" + pass);
+  console.log("----------- ");
+}
+login("zalfa", "********");
